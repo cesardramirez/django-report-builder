d3cd82c602ae6e101fad6169385681cedba2bc9f
cramirezd <cesardavid89@gmail.com>
2017-04-11 13:12:01 -0500

    Mejora Traduccion App Report Builder

--- report_builder/apps.py
+++ report_builder/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 class ReportBuilderConfig(AppConfig):
     name = 'report_builder'
-    verbose_name = 'Reportes'
+    verbose_name = _('Reports')
